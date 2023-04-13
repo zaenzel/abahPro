9718e1d1d87a22abb3212ae6bcf6bfe21e6e3202
zaenzel <zaenzel21@gmail.com>
2023-04-13 17:30:32 +0700

    add dummy data

--- src/view/detail/Detail.jsx
+++ src/view/detail/Detail.jsx
@@ -5,40 +5,47 @@ import home2 from "/images/townhouse.jpg";
 import home3 from "/images/home.jpg";
 import IconText from "../../components/icon-text/IconText";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import SingleBedIcon from "@mui/icons-material/SingleBed";
-import BathtubIcon from "@mui/icons-material/Bathtub";
-import ZoomOutMapIcon from "@mui/icons-material/ZoomOutMap";
 import Button from "../../components/button/Button";
 import WrapIconPro from "../../components/wrap-icon-properti/WrapIconPro";
+import { useParams } from "react-router-dom";
+import properti from "../../properti";
 
 const Detail = () => {
+  const { id } = useParams();
+  const dataPro = properti;
   const images = [home1, home2, home3];
+
+  let rupiah = Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
 
   return (
     <div className="md:flex md:container md:mx-auto">
       <div className="basis-2/3 space-y-5">
         <Carousel images={images} />
         <div className="desc-detail">
-          <h1 className="title-desc">Go Home - Alam Sutera</h1>
+          <h1 className="title-desc">{dataPro[id - 1].name}</h1>
           <IconText gray={true}>
             <LocationOnIcon />
-            <p className="">Serpong, Tanggerang</p>
+            <p className="">{dataPro[id - 1].location}</p>
           </IconText>
-          <p className="price-detail">Rp 800 juta</p>
-          <WrapIconPro />
+          <p className="price-detail">{rupiah.format(dataPro[id - 1].price)}</p>
+          <WrapIconPro
+            kt={dataPro[id - 1].bedroom}
+            km={dataPro[id - 1].bathroom}
+            area={dataPro[id - 1].area}
+          />
           <div className="desc">
             <h1 className="text-xl">Deskripsi</h1>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Doloribus laborum ad, illo quidem sunt quasi quisquam nostrum
-              consectetur ipsam totam cupiditate ullam illum quis excepturi
-              laudantium saepe necessitatibus at? Officia?
+             {dataPro[id-1].desc}
             </p>
           </div>
         </div>
       </div>
 
-      <aside className="container mx-auto px-3 mt-10 md:mt-0 basis-1/3">
+      <aside className="container mx-auto px-3 mt-10 md:mt-0 basis-1/3 min-w-fit">
         <div className="p-5 text-center space-y-3 border shadow-sm rounded-lg">
           <h2 className="text-xl lg:text-2xl font-bold">Ingin tanya-tanya?</h2>
           <h5 className="text-color-ADADAD text-sm">
