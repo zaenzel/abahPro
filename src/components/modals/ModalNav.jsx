31e4057fdffea47ee49a1ef733c424a34e68f075
zaenzel <zaenzel21@gmail.com>
2023-04-23 11:06:15 +0700

    add nav in modal nav

--- src/components/modals/ModalNav.jsx
+++ src/components/modals/ModalNav.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Button from "../button/Button";
 import "./modalNav.css";
+import { useNavigate } from "react-router-dom";
 
 const ModalNav = ({ menu }) => {
+  const navigate = useNavigate();
+
+  const clicked = (link) => {
+    navigate(link), (menu = "menu hide");
+  };
+
   return (
     <div className={menu}>
       <ul className="wrap-items">
-        <li>Daftar Propeti</li>
-        <li>Tentang Kami</li>
+        <li onClick={(e) => clicked("listproperti")}>Daftar Propeti</li>
+        <li onClick={(e) => clicked("listblog")}>Blog</li>
         <li>
           <Button word={"Hubungi Kami"} />
         </li>
